f0fd7bc38b50503954be16ade8c57a4191dd6f37
Blaine Kennedy <blainekennedy@gmail.com>
2023-07-11 17:55:23 -0500

    Complete if_statements.rb

--- if_statements.rb
+++ if_statements.rb
@@ -43,8 +43,11 @@ end
 
 num_quarters = 0
 
+if num_quarters >= 2
 puts "I have enough money for a gumball"
+else num_quarters < 2
 puts "I don't have enough money for a gumball"
+end
 
 
 #####################
@@ -61,5 +64,11 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 and has_sauce == true
+  puts "I can make pizza"
+else cups_of_flour <= 1 or has_sauce == false
+  puts "I cannot make pizza"
+end
